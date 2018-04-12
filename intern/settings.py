530f6c8ea7d84750d5da9c853a08d7973f1b3faa
Amit <amitsharma.sgr99@gmail.com>
2018-04-12 23:44:41 +0530

    ASd

--- intern/settings.py
+++ intern/settings.py
@@ -16,7 +16,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -26,7 +25,7 @@ SECRET_KEY = '%38q^@(7aag$nu0ti7zv3=l(q(!zg07_m2y*b%s=2u!q_!o=_u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://demo-bootcamptesttable.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['']
 
 # Application definition
 
@@ -42,7 +41,7 @@ INSTALLED_APPS = [
 
 ]
 
-import whitenoise
+# import whitenoise
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -51,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'intern.urls'
@@ -79,17 +78,17 @@ WSGI_APPLICATION = 'intern.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-import dj_database_url
 DATABASES = {
-   'default':dj_database_url.config(default='postgres://bfzltieyybnfst:351098570baf6c806d1bb53fe605c87293d2769ba4d1f798f0ad055d9d606cb0@ec2-54-83-1-94.compute-1.amazonaws.com:5432/d1n9u0j31il66f')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+# import dj_database_url
+# DATABASES = {
+#    'default':dj_database_url.config(default='postgres://bfzltieyybnfst:351098570baf6c806d1bb53fe605c87293d2769ba4d1f798f0ad055d9d606cb0@ec2-54-83-1-94.compute-1.amazonaws.com:5432/d1n9u0j31il66f')
+# }
 
 
 # Password validation
@@ -151,7 +150,3 @@ django.setup()
 # db_from_env = dj_database_url.config()
 # DATABASES['default'].update(db_from_env)
 # DATABASES['default']['CONN_MAX_AGE'] = 500
-import django_heroku
-django_heroku.settings(locals())
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
